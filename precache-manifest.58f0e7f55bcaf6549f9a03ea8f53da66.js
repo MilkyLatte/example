9e87b47f6c59f90fa6c464bb62802404439f25f7
MilkyLatte <jpqo9508@gmail.com>
2019-04-08 22:30:10 +0100

    Updates

---
+++ precache-manifest.58f0e7f55bcaf6549f9a03ea8f53da66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8504fab84e7b6f9011ec",
+    "url": "/example/static/css/main.e8c65b78.chunk.css"
+  },
+  {
+    "revision": "8504fab84e7b6f9011ec",
+    "url": "/example/static/js/main.986badd7.chunk.js"
+  },
+  {
+    "revision": "91c2a3b6d402a5254b11",
+    "url": "/example/static/js/runtime~main.3b33d9c4.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/example/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd1207c5421bd970949e77c03a248a38",
+    "url": "/example/index.html"
+  }
+];
